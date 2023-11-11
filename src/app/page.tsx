8368bd3c9e772c088452e81338cc816ec6833c8f
Felipe Scherer <felipescherer25@gmail.com>
2023-11-11 16:11:56 -0300

    :sparkles: feat

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            By a {' '}
+            By a dsadasdasd{' '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"

7c918555ecd55d66f78dd127a88259cd6d12a7fa
Felipe Scherer <felipescherer25@gmail.com>
2023-11-11 16:24:21 -0300

    :test:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,8 +14,8 @@ export default function Home() {
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
-          >
-            By a dsadasdasd{' '}
+          > 
+            By a daasdasd dsadasdasd{' '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"

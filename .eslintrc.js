5a6b0fde9af037dc9efd8f058eac73771079f8fa
Felipe Scherer <felipescherer25@gmail.com>
2023-12-28 17:16:54 -0300

    :sparkles: feat: eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@stylistic/recommended-extends',
+    'plugin:tailwindcss/recommended',
+  ],
+  plugins: ['react', '@stylistic'],
+  parserOptions: {
+    ecmaFeatures: { jsx: true },
+  },
+  rules: {
+    'react/react-in-jsx-scope': 'off',
+    '@stylistic/array-element-newline': ['warn', 'consistent'],
+    '@stylistic/array-bracket-newline': [
+      'warn', {
+        multiline: true,
+      },
+    ],
+    '@stylistic/brace-style': [
+      'warn', '1tbs', {
+        allowSingleLine: false,
+      },
+    ],
+  },
+}

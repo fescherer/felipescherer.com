366bfe1c0de2f7f74b57344d5f1be55b65e86285
Felipe Scherer <felipescherer25@gmail.com>
2023-12-28 17:48:02 -0300

    :wrench: add pnpm as dependency

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
-import Link from 'next/link'
-
 export default function Home() {
   return (
     <main className="m-1 flex">
       <span>dsadasdas</span>
-      <Link href="/test"> Ir ao teste</Link>
     </main>
   )
 }

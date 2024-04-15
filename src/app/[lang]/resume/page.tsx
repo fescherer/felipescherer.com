7a77c17753517d568ce2db1cdf9ff6c9e83a03de
Felipe Scherer <felipescherer25@gmail.com>
2024-04-15 10:22:30 -0300

    :sparkles: feat resume translation

--- src/app/[lang]/resume/page.tsx
+++ src/app/[lang]/resume/page.tsx
@@ -1,4 +1,5 @@
 import { Resume } from '@/features'
+import { getDictionary } from '@/get-dictionary'
 import { RootLayoutType } from '@/types/Page'
 import { getMetadata } from '@/utils/functions/getMetada'
 import { Metadata } from 'next'
@@ -12,8 +13,18 @@ export const metadata: Metadata = getMetadata({
   defaultKeywords: ['resume', 'cv'],
 })
 
-export default function Page({ params }: RootLayoutType) {
+export default async function Page({ params }: RootLayoutType) {
+  const dictionary = await getDictionary(params.lang)
+
   return (
-    <Resume lang={params.lang} />
+    <Resume
+      lang={params.lang}
+      translations={{
+        'title': dictionary.resume.title,
+        'text': dictionary.resume.text,
+        'en-version': dictionary.resume['en-version'],
+        'pt-version': dictionary.resume['pt-version'],
+      }}
+    />
   )
 }
--- src/features/Resume/index.tsx
+++ src/features/Resume/index.tsx
@@ -5,22 +5,26 @@ import { useState } from 'react'
 
 type ResumeProps = {
   lang: Locale
+  translations: {
+    title: string
+    text: string
+    'en-version': string
+    'pt-version': string
+  }
 }
 
-export function Resume({ lang }: ResumeProps) {
+export function Resume({ lang, translations }: ResumeProps) {
   const [selectedLanguage, setSelectedLanguage] = useState(lang)
 
   return (
     <div className="m-auto my-4 flex max-w-5xl flex-col gap-2">
-      <h2 className="font-title text-3xl text-on-layer-0-l1">View/Download my Resume</h2>
-      <p>Thank you for your interest in my work. By clicking on the download button, you&apos;ll gain access to a detailed document that highlights my educational background, work history, key achievements, and areas of expertise. Whether you&apos;re a potential employer, collaborator, or simply curious about my career trajectory, my CV/resume offers valuable insights.</p>
+      <h2 className="font-title text-3xl text-on-layer-0-l1">{translations.title}</h2>
+      <p>{translations.text}</p>
 
       <div className="flex flex-col justify-between gap-2 md:flex-row">
-        <p>
-          You are seeing the
-          <strong className="text-brand-primary">{` ${i18nWithName.locales.find(locale => locale.id === selectedLanguage)?.name} `}</strong>
-          version.
-        </p>
+        <strong className="font-medium">
+          {selectedLanguage === 'en' ? translations['en-version'] : translations['pt-version']}
+        </strong>
 
         <div className="flex gap-1 rounded border border-layer-2">
           {i18nWithName.locales.map(language => (

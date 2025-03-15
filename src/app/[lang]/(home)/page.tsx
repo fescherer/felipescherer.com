9b9e8e262d8c0f48c48db4fd8a7679b21bba87c2
unknown <felipescherer25@gmail.com>
2025-03-15 17:09:23 -0300

    :sparkles: feat toolkit part

--- src/app/[lang]/(home)/page.tsx
+++ src/app/[lang]/(home)/page.tsx
@@ -11,10 +11,10 @@ import { LucideArrowDown } from 'lucide-react'
 import QRCode from 'react-qr-code'
 import { ContentContainer } from './components/content-container.component'
 import { Dialog } from '@/lib/_felipescherer.com/components/primitives'
-import { TOOLKIT } from '@/lib/_felipescherer.com/data/toolkit.data'
 import { ProjectsShowcaseComponent } from '@/lib/_felipescherer.com/components/sections/projects/projects-showcase.component'
 import { getServicesData } from '@/@data/services.data'
 import { getSoftSkillsData } from '@/@data/soft-skills.data'
+import { getToolkitData } from '@/@data/toolkit.data'
 
 export default async function HomePage({ params: { lang } }: RootLayoutType) {
   const dictionary = await getDictionary(lang)
@@ -22,6 +22,7 @@ export default async function HomePage({ params: { lang } }: RootLayoutType) {
 
   const SERVICES_DATA = await getServicesData(lang)
   const SOFT_SKILLS_DATA = await getSoftSkillsData(lang)
+  const TOOLKIT_DATA = await getToolkitData(lang)
 
   return (
     <div className="m-auto pt-8 max-w-4xl">
@@ -151,24 +152,24 @@ export default async function HomePage({ params: { lang } }: RootLayoutType) {
         <ContentContainer title="Toolbox">
           <div className="flex flex-wrap justify-center gap-8">
             {
-              TOOLKIT.map(tool => (
+              TOOLKIT_DATA.map(tool => (
                 <Dialog
                   classContent="max-w-[450px]"
-                  key={tool.name}
+                  key={tool.title}
                   trigger={(
-                    <button type="button" className="flex h-full w-24 flex-col items-center gap-4 transition-all hover:scale-110 hover:text-on-layer-0-l1">
-                      <div className="rounded-sm bg-[#fff] p-1">
+                    <button type="button" className="w-24 h-36 card-primary rounded flex flex-col items-center">
+                      <div className="bg-white w-full flex justify-center p-4">
                         {tool.icon}
                       </div>
 
-                      <p className="flex-1">{tool.name}</p>
+                      <p className="flex items-center h-full">{tool.title}</p>
                     </button>
                   )}
                 >
                   <div className="relative flex flex-col gap-8 rounded-sm bg-layer-1 p-2">
-                    <p className="flex-1">{tool.description[lang]}</p>
+                    <p className="flex-1">{tool.text}</p>
 
-                    <Link href={tool.docLink} target="_blank" className="mt-4 flex items-center gap-2 text-sm transition-all hover:brightness-90">
+                    <Link href={tool.docLink} target="_blank" className="mt-4 flex items-center gap-2 text-sm primary-link-effect w-fit">
                       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="size-4">
                         <path fillRule="evenodd" d="M4 2a1.5 1.5 0 0 0-1.5 1.5v9A1.5 1.5 0 0 0 4 14h8a1.5 1.5 0 0 0 1.5-1.5V6.621a1.5 1.5 0 0 0-.44-1.06L9.94 2.439A1.5 1.5 0 0 0 8.878 2H4Zm1 5.75A.75.75 0 0 1 5.75 7h4.5a.75.75 0 0 1 0 1.5h-4.5A.75.75 0 0 1 5 7.75Zm0 3a.75.75 0 0 1 .75-.75h4.5a.75.75 0 0 1 0 1.5h-4.5a.75.75 0 0 1-.75-.75Z" clipRule="evenodd" />
                       </svg>

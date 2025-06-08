dfac0e6fbd0167af6c6f5571a19be87ce0c3558c
unknown <felipescherer25@gmail.com>
2025-06-08 12:45:59 -0300

    :sparkles: improve project page

--- src/@components/projects/components/project-list/components/card.component.tsx
+++ src/@components/projects/components/project-list/components/card.component.tsx
@@ -1,3 +1,4 @@
+import { YMotion } from '@/@components/motion/y-motion.component'
 import { IProject } from '@/@data/projects.data'
 import { Tooltip } from '@/lib/primitives'
 import Link from 'next/link'
@@ -11,21 +12,23 @@ export function Card({ project }: CardProps) {
   const imagePath = `/projects/${project.type.id}/${project.id}`
 
   return (
-    <Tooltip tooltip={project.title}>
-      <Link href={linkPath} className="bg-base-200 h-fit overflow-hidden hover:scale-105 transition-all relative rounded border-primary/30 border shadow-md">
-        <div className="absolute size-full scale-110 bg-cover bg-center blur-xs" style={{ backgroundImage: `url(${imagePath}/thumb.webp)` }} />
+    <YMotion className='bg-base-200 overflow-hidden hover:scale-105  h-fit relative rounded transition-[scale] border-primary/30 border shadow-md'>
+      <Tooltip tooltip={project.title}>
+        <Link href={linkPath} className="" >
+          <div className="absolute size-full scale-110 bg-cover bg-center blur-xs" style={{ backgroundImage: `url(${imagePath}/thumb.webp)` }} />
 
-        <div className="h-50 relative">
-          <div className="absolute top-0 w-full h-full bg-contain bg-center bg-no-repeat" style={{ backgroundImage: `url(${imagePath}/thumb.webp)` }} />
-        </div>
+          <div className="h-50 relative">
+            <div className="absolute top-0 w-full h-full bg-contain bg-center bg-no-repeat" style={{ backgroundImage: `url(${imagePath}/thumb.webp)` }} />
+          </div>
 
-        <div className="p-4 text-primary-content bg-primary/70 relative">
-          <h2 className="font-medium text-lg text-ellipsis whitespace-nowrap overflow-hidden font-title pb-2">{project.title}</h2>
-          <p className="vertical-ellipsis mt-2 text-sm mb-4">{project.description}</p>
-          <span className="text-sm">Read more...</span>
-        </div>
+          <div className="p-4 text-primary-content bg-primary/70 relative">
+            <h2 className="font-medium text-lg text-ellipsis whitespace-nowrap overflow-hidden font-title pb-2">{project.title}</h2>
+            <p className="vertical-ellipsis mt-2 text-sm mb-4">{project.description}</p>
+            <span className="text-sm">Read more...</span>
+          </div>
 
-      </Link>
-    </Tooltip>
+        </Link >
+      </Tooltip>
+    </YMotion>
   )
 }
--- src/@components/projects/components/project-list/project-list.component.tsx
+++ src/@components/projects/components/project-list/project-list.component.tsx
@@ -1,16 +1,14 @@
 'use client'
 
-import { useEffect, useState } from 'react'
-import { LucideChevronLeft, LucideChevronRight } from 'lucide-react'
 import { PropsWithLocale } from '@/@types/language'
 import { useSearchContext } from '../../context/search.context'
 import { Card } from './components/card.component'
 import { IProject } from '@/@data/projects.data'
+import { YMotion } from '@/@components/motion/y-motion.component'
 
 type ProjectListProps = {
   projects: IProject[]
 }
-const PAGES_NUMBER = 6
 
 function getProjectsSortedByDate(ProjectA: IProject, ProjectB: IProject) {
   const dateProjectA = new Date(ProjectA?.date)
@@ -27,56 +25,23 @@ function getProjectsSortedByDate(ProjectA: IProject, ProjectB: IProject) {
 
 export function ProjectList({ projects }: PropsWithLocale<ProjectListProps>) {
   const { search } = useSearchContext()
-  const [currentPage, setCurrentPage] = useState(0)
-
-  useEffect(() => {
-    setCurrentPage(0)
-  }, [search])
 
   const safeFilter = search.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')
   const rg = new RegExp(`^(?=.*\\b${safeFilter}).*$`, 'gmi')
 
   const filteredProjects = search
-    ? projects.filter(project => project.id.match(rg)).sort(getProjectsSortedByDate)
+    ? projects.filter(project => project.id.match(rg) || project.title.match(rg)).sort(getProjectsSortedByDate)
     : projects.sort(getProjectsSortedByDate)
-
-  const pages = Array.from(Array(Math.ceil(filteredProjects.length / PAGES_NUMBER)).keys())
-
-  const handleChange = (value: number, specificPage: boolean = false) => {
-    if (specificPage) setCurrentPage(value)
-    else
-      setCurrentPage(prev => prev + value)
-    window.scrollTo({
-      top: 0,
-      left: 0,
-      behavior: 'smooth',
-    })
-  }
 
   return (
     <>
-      <div className="grid grid-cols-1 gap-10 sm:grid-cols-2 md:min-h-[680px] lg:grid-cols-3 p-4">
+      <div className="grid grid-cols-1 gap-x-10 gap-y-16 sm:grid-cols-2 md:min-h-[680px] lg:grid-cols-3 p-4">
         {
-        filteredProjects.slice(currentPage * PAGES_NUMBER, currentPage * PAGES_NUMBER + PAGES_NUMBER).map(project => (
-          <Card key={project.id} project={project} />
-        ))
-      }
+          filteredProjects.map(project => (
+            <Card key={project.id} project={project} />
+          ))
+        }
       </div>
-
-      { filteredProjects.length > PAGES_NUMBER && (
-        <div className="mt-8 flex items-center justify-end gap-2">
-          <button onClick={() => handleChange(-1)} disabled={currentPage === 0} type="button" className="rounded-full transition-all hover:enabled:text-accent hover:enabled:cursor-pointer disabled:cursor-not-allowed disabled:opacity-30">
-            <LucideChevronLeft />
-          </button>
-
-          {pages.map(page => <button onClick={() => handleChange(page, true)} type="button" className={`flex size-8 cursor-pointer items-center justify-center rounded-full transition-all hover:bg-accent hover:text-accent-content ${currentPage === page ? 'bg-primary text-primary-content' : ''}`} key={page}>{page + 1}</button>)}
-
-          <button onClick={() => handleChange(1)} disabled={currentPage === pages.length - 1} type="button" className="rounded-full transition-all hover:enabled:text-accent hover:enabled:cursor-pointer disabled:cursor-not-allowed disabled:opacity-30">
-            <LucideChevronRight />
-          </button>
-        </div>
-      )}
-
     </>
   )
 }
